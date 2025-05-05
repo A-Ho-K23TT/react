ac30f39836168214791c5462e114dcc2259de866
A-Ho-K23TT <ahho.k23tt@kontum.udn.vn>
2025-05-05 16:07:53 +0700

    Add files via upload

---
+++ Bai2/main2.js
@@ -0,0 +1,14 @@
+
+// cau 2a
+const doubleArray = (arr) => arr.map(num => num * 2);
+console.log(doubleArray([1, 2, 3])); 
+
+// 2b
+const person2 = {
+    name: "udck",
+    printName: () => {
+      console.log("Tên là: " + person2.name);
+    }
+  };
+  
+  person2.printName(); 
